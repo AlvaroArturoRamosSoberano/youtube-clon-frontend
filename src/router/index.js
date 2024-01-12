884199c7c1e17fc35fcd8942ab71691299020dd0
AlvaroArturoRamosSoberano <alvarors935@gmail.com>
2024-01-12 17:03:03 -0600

    Library added: Element+

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import AppVue from '@/App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: HomeView
     },
   ]
 })
